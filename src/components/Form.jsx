36af91709ce5d533ee7e168bde741691c90629fa
Benjamin Janusek <bjanusek00@gmail.com>
2025-09-03 12:41:49 -0700

    React interactivity: Events and state
    - Handling events
    
    - Callback props
    
    - Persisting and changing data with state
    
    - Putting it all together: Adding a task
    
    - Detour: counting tasks
    
    - Completing a task
    
    - Deleting a task
    
    - Deleting tasks from state and UI
    
    Summary:
    
     That's enough for one article. Here we've given you the lowdown on how React deals with events and
     handles state, and implemented functionality to add tasks, delete tasks, and toggle tasks as
     completed. We are nearly there. In the next article we'll implement functionality to edit existing tasks
     and filter the list of tasks between all, completed, and incomplete tasks. We'll look at conditional UI
     rendering along the way.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,19 @@
-function Form() {
+import { useState } from "react";
 
-    function handleSubmit(event) {
-        event.preventDefault();
-        alert("Hello, world!");
-    }
+function Form(props) {
+
+  const [name, setName] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    props.addTask(name);
+    setName("");
+  }
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
 
   return (
     <form onSubmit={handleSubmit}>
@@ -18,6 +28,8 @@ function Form() {
         className="input input__lg"
         name="text"
         autoComplete="off"
+        value={name}
+        onChange={handleChange}
       />
       <button type="submit" className="btn btn__primary btn__lg">
         Add
